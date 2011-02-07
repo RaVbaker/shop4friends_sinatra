c0d61ecef71bae9ff8b4ea017d3201d00a625ca1
Rafal Piekarski <ravbaker@gmail.com>
2011-02-07 23:18:28 +0100

    fix for global vars...

--- app.rb
+++ app.rb
@@ -11,12 +11,6 @@ enable :sessions
 
 require 'simplefb'
 
-fb = SimpleFB.new(  
-  :api_key  => 'aac29c38637b6351a2694f551adf4756',
-  :secret   => '1a48af091b8cfdd32b344f99706295f8',
-  :app_id   => 191820210836125, 
-  :scope => "email,user_about_me,user_events,friends_activities,user_activities,friends_hometown,friends_interests,friends_likes,friends_location,friends_status,friends_checkins"
-)
      
 
 #amazon config:
@@ -31,12 +25,18 @@ search = Gifts.new(
 #app:         
 
 before {               
-  fb.params[:redirect_uri] = "http://#{request.host}#{':'+request.port.to_s if request.port != 80}/auth"
+  @fb = fb = SimpleFB.new(  
+    :api_key  => 'aac29c38637b6351a2694f551adf4756',
+    :secret   => '1a48af091b8cfdd32b344f99706295f8',
+    :app_id   => 191820210836125, 
+    :redirect_uri => "http://#{request.host}#{':'+request.port.to_s if request.port != 80}/auth",
+    :scope => "email,user_about_me,user_events,friends_activities,user_activities,friends_hometown,friends_interests,friends_likes,friends_location,friends_status,friends_checkins"
+  )
+  
+  
   # load FB accesstoken and code if exists
-  fb.code = session['code'] if session['code'] && !params[:code]
-  fb.access_token = session['access_token'] if !params[:code] && session['access_token']
-  p fb.access_token, fb.params[:code]
-  @fb = fb    
+  @fb.code = session['code'] if session['code'] && !params[:code]
+  @fb.access_token = session['access_token'] if !params[:code] && session['access_token']
   @me = session['user'] if session['user']
 }
 
@@ -51,15 +51,15 @@ end
 get '/auth' do                                                                               
   session['code'] = session['access_token'] = request.cookies[:access_token] = nil
   session['code'] = params[:code]
-  fb.code = params[:code]
-  session['access_token'] = request.cookies[:access_token] = fb.access_token     
-  session['user'] = fb.me
-  redirect '/select_friend' if fb.access_token
+  @fb.code = params[:code]
+  session['access_token'] = request.cookies[:access_token] = @fb.access_token     
+  session['user'] = @fb.me
+  redirect '/select_friend' if @fb.access_token
   redirect '/'
 end
 
 get '/select_friend' do 
-  @friends = fb.me_friends :fields => "id,name,link" 
+  @friends = @fb.me_friends :fields => "id,name,link" 
   @begin = params[:start].to_i || 0
   @limit = 1000               
   @me = session['user']             
@@ -73,10 +73,10 @@ get "/favicon.ico" do
 end   
 
 get '/:fb_id' do
-  @user = fb.get(params[:fb_id])
+  @user = @fb.get(params[:fb_id])
   @me = session['user']
      
-  all_likes = fb.get("#{params[:fb_id]}/likes", {:fields => "id,name,link,category"})['data']                             
+  all_likes = @fb.get("#{params[:fb_id]}/likes", {:fields => "id,name,link,category"})['data']                             
       
   @likes = all_likes.shuffle[1..50]    
   
@@ -96,7 +96,7 @@ get '/:fb_id' do
 end                     
 
 get "/:fb_id/sorry" do
-  @user = fb.get(params[:fb_id])
+  @user = @fb.get(params[:fb_id])
   @me = session['user']
   haml :user_sorry
 end
